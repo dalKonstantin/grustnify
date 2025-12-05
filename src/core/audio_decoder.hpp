d0b85fa96aa5df140ca84abe62cb09100fdb7252
Константин Даль <djkostjan@MacBook-Pro-Konstantin.local>
2025-12-05 16:12:06 +0300

    Decode to  buffer need to test on x64, cannot build on arm64

--- src/core/audio_decoder.hpp
+++ src/core/audio_decoder.hpp
@@ -4,10 +4,13 @@
 
 extern "C" {
 #include <libavcodec/avcodec.h>
+#include <libavcodec/codec.h>
 #include <libavcodec/packet.h>
 #include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
 #include <libavutil/channel_layout.h>
-#include <libavutil/samplefmt.h>
+#include <libavutil/error.h>
+#include <libavutil/frame.h>
 #include <libswresample/swresample.h>
 }
 
@@ -20,15 +23,23 @@ public:
   bool decode_to_buffer(core::AudioBuffer &buffer);
 
 private:
+  bool init_resampler();
+
+private:
   QString path_ = "";
   AVFormatContext *format_ctx_ = nullptr;
   AVCodecContext *codec_ctx_ = nullptr;
+  SwrContext *swr_ctx_ = nullptr;
   int audio_stream_index_ = -1;
   AVChannelLayout input_channel_layout_;
   AVChannelLayout output_channel_layout_;
+  AVFrame *frame_ = nullptr;
 
   int input_sample_rate_ = 0;
   int output_sample_rate_ = 0;
   int output_channels_ = 0;
+  bool end_of_file_ = false;
+  AVSampleFormat output_sample_fmt_ = AV_SAMPLE_FMT_FLT;
+  AVPacket *packet_;
 };
 } // namespace core
